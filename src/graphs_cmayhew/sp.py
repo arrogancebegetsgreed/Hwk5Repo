6fe81cae15de7eee34e1179e795c5db371c18ebe
Caine <crmquaffle@gmail.com>
2026-09-16 21:03:27 -0600

    save

--- src/graphs_cmayhew/sp.py
+++ src/graphs_cmayhew/sp.py
@@ -1,27 +1,19 @@
-from graphs_tmota import sp
 import sys
+from heapq import heappush, heappop
 
-if __name__ == '__main__':
-    
-    if len(sys.argv) != 2:
-        print(f'Use: {sys.argv[0]} graph_file')
-        sys.exit(1)
+def dijkstra(graph, source):
+    dist = {node: sys.maxsize for node in graph}
+    dist[source] = 0
+    heap = []
+    heappush(heap, (0, source))
+    path = {source: []}
 
-    graph = {}
-    with open(sys.argv[1], 'rt') as f:
-        for line in f:
-            line = line.strip()
-            s, d, w = line.split()
-            s = int(s)
-            d = int(d)
-            w = int(w)
-            if s not in graph:
-                graph[s] = {}
-            graph[s][d] = w
-    
-    s = 0
-    dist, path = sp.dijkstra(graph, s)
-    print(f'Shortest distances from {s}:')
-    print(dist)
-    for d in path: 
-        print(f'spf to {d}: {path[d]}')
+    while heap:
+        w, u = heappop(heap)
+        for v in graph[u]:
+            if w + graph[u][v] < dist[v]:
+                dist[v] = w + graph[u][v]
+                heappush(heap, (dist[v], v))
+                path[v] = path[u] + [u]
+
+    return dist, path
